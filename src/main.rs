fc1e3bcc15373fd43a632ce4bdebd52853097bd2
Tristan Murphy <tristan.murphy@outlook.com>
2025-01-17 17:20:41 -0500

    make it properly stop at the required amount of data instead of overshooting when the total data isnt cleanly divisible by the buffer size

--- src/main.rs
+++ src/main.rs
@@ -38,26 +38,24 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .progress_chars("#>-"));
 
     let mut total_written = 0;
-    // let mut iter_count = 0;
     while total_written < args.total_data {
         // allocating and then storing is quite a bit faster i think
-        let mut buf = vec![0; args.buf_size];
+        let remaining = args.total_data - total_written;
+        let mut buf = if args.buf_size as u64 <= remaining {
+            vec![0; args.buf_size]
+        } else {
+            vec![0; remaining as usize]
+        };
 
         buf
             .par_iter_mut()
             .for_each(|b| *b = fastrand::u8(0..u8::MAX));
-        // let buf: Vec<_> = (0..args.buf_size)
-        //     .into_par_iter()
-        //     .map(|_| fastrand::u8(0..u8::MAX))
-        //     .collect();
         
         let written = file.write(&buf).await?;
         total_written += written as u64;
         // iter_count += 1;
         pb.set_position(total_written);
     }
-    
-    // println!("avg successful bytes written: {}", total_written as f32 / iter_count as f32);
 
     pb.finish();
 
